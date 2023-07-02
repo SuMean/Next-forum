e3e63f108bc27a394540ac8df86791fea671b4b2
sam101321 <sam010321@gmail.com>
2023-07-03 01:03:17 +0900

    db setting

---
+++ util/database.tsx
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+declare const global: any;
+const url =
+  "mongodb+srv://admin:qwe123!!@cluster0.azzwzth.mongodb.net/?retryWrites=true&w=majority";
+
+let connectDB: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === "development") {
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url).connect();
+}
+export { connectDB };
