089aaf7d3a307a035347a1eeecb1f74ee892d222
sam101321 <sam010321@gmail.com>
2023-07-07 14:24:06 +0900

    server api

---
+++ pages/api/list.tsx
@@ -0,0 +1,6 @@
+import { connectDB } from "@/util/database";
+export default async function handler(요청, 응답) {
+  let db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+  응답.status(200).json(result);
+}
