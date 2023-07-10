ad3831e03704a885a6d9808fa7411f343117dc90
sam101321 <sam010321@gmail.com>
2023-07-10 12:30:53 +0900

    destructuring

--- app/list/ListItem.tsx
+++ app/list/ListItem.tsx
@@ -2,17 +2,18 @@
 
 import Link from "next/link";
 
-export default function ListItem(props:any) {
+export default function ListItem({result}:any) {
   return (
     <div>
-      {props.result.map((a:any, i:number) => (
+      {result.map((a: any, i: number) => (
         <div className="list-item" key={i}>
-          <Link href={"/detail/" + props.result[i]._id}>
-            <h4>{props.result[i].title}</h4>
+          <Link href={"/detail/" + result[i]._id}>
+            <h4>{result[i].title}</h4>
           </Link>
-          <Link href={"/edit/" + props.result[i]._id} className="list-btn">
+          <Link href={"/edit/" + result[i]._id} className="list-btn">
             ✏️
           </Link>
+          <span>🗑️</span>
           <p>1월 1일</p>
         </div>
       ))}
