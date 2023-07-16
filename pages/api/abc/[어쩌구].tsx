2f87548e1588c850b4008c1a29c57886704f5ee7
sam101321 <sam010321@gmail.com>
2023-07-16 23:27:48 +0900

    파일정리

--- pages/api/abc/[어쩌구].tsx
+++
@@ -1,4 +0,0 @@
-export default function handler(요청: any, 응답: any) {
-  console.log(요청.query);
-  응답.status(200).json("처리완료");
-}
--- pages/api/date.tsx
+++
@@ -1,4 +0,0 @@
-export default function handler(요청: any, 응답: any) {
-  let date = new Date();
-  응답.status(200).json(date);
-}
