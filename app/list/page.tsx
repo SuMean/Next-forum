a399e1b79d8a8158862d383126723017a95e94c0
sam101321 <sam010321@gmail.com>
2023-07-17 15:32:47 +0900

    cache

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -1,17 +1,16 @@
 import { connectDB } from "@/util/database";
 import ListItem from "./ListItem";
 
-export const dynamic = "force-dynamic"
+// export const revalidate = 20;
 
 export default async function List() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
-  result = result.map((a:any) => {
+  result = result.map((a: any) => {
     a._id = a._id.toString();
     return a;
   });
   console.log(result);
-  
 
   return (
     <div className="list-bg">
