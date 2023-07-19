3d784ece817778fde671167660185e62788bdf17
sam101321 <sam010321@gmail.com>
2023-07-19 23:52:42 +0900

    test

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -9,7 +9,7 @@ export default async function Detail(props: { params: { id: string } }) {
   // console.log(props.params.id);
   return (
     <div>
-      <h4>상세페이지임</h4>
+      <h4>상세페이지</h4>
       <h4>{result?.title}</h4>
       <p>{result?.content}</p>
     </div>
