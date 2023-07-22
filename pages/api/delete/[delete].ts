e51fd3d94744ffd4b5ad55576fadbaf4f1326576
sam101321 <sam010321@gmail.com>
2023-07-22 23:25:14 +0900

    feat(delete)인증유저 글 삭제

--- pages/api/delete/[delete].ts
+++ pages/api/delete/[delete].ts
@@ -1,13 +1,22 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler(요청: any, 응답: any) {
   if (요청.query) {
+    let session = await getServerSession(요청, 응답, authOptions);
+
     let db = (await connectDB).db("forum");
-    let result = await db
+    let find = await db
       .collection("post")
-      .deleteOne({ _id: new ObjectId(요청.query) });
-    console.log(result);
-    응답.status(200).json("삭제완료");
+      .findOne({ _id: new ObjectId(요청.query) });
+    if (session?.user?.email === find?.author) {
+      let result = await db
+        .collection("post")
+        .deleteOne({ _id: new ObjectId(요청.query) });
+      console.log(result);
+      응답.status(200).json("삭제완료");
+    }
   }
 }
--- pages/api/post/new.ts
+++ pages/api/post/new.ts
@@ -4,14 +4,15 @@ import { getServerSession } from "next-auth";
 
 export default async function handler(요청: any, 응답: any) {
   let session = await getServerSession(요청, 응답, authOptions);
+  // console.log(session?.user!.email)
   if (session) {
-    요청.body.author = session.user?.email;
+    요청.body.author = session.user!.email;
   }
   console.log(요청.body);
 
   if (요청.method == "POST") {
     if (요청.body.title == "") {
-      return 응답.status(500).json("제목써라");
+      return 응답.status(500).json("제목써주세용");
     }
     let db = (await connectDB).db("forum");
     let result = db.collection("post").insertOne(요청.body);
