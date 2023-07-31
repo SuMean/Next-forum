7c3bbae66602f8eb356d0a4ba66636b6b98301dc
sam101321 <sam010321@gmail.com>
2023-07-31 15:59:23 +0900

    metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import { connectDB } from "../util/database";
 
+export const metadata = {
+  title: " next-forum",
+  description: "게시판입니다",
+};
+
+
 export default async function Home() {
   let db = (await connectDB).db("forum");
   let result = await db.collection("post").find().toArray();
