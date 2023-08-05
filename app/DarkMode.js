08afc2e7a93f742afed982b6612d414b170d28ea
sam101321 <sam010321@gmail.com>
2023-08-05 22:10:44 +0900

    fix(Darkmode)변수변경

--- app/DarkMode.js
+++ app/DarkMode.js
@@ -6,8 +6,11 @@ export default function DarkMode() {
   let router = useRouter();
 
   useEffect(() => {
-    let 쿠키값 = ("; " + document.cookie).split(`; mode=`).pop().split(";")[0];
-    if (쿠키값 == "") {
+    let changeMode = ("; " + document.cookie)
+      .split(`; mode=`)
+      .pop()
+      .split(";")[0];
+    if (changeMode == "") {
       document.cookie = "mode=light; max-age=" + 3600 * 24 * 400;
     }
   }, []);
@@ -15,11 +18,11 @@ export default function DarkMode() {
   return (
     <span
       onClick={() => {
-        let 쿠키값 = ("; " + document.cookie)
+        let changeMode = ("; " + document.cookie)
           .split(`; mode=`)
           .pop()
           .split(";")[0];
-        if (쿠키값 == "light") {
+        if (changeMode == "light") {
           document.cookie = "mode=dark; max-age=" + 3600 * 24 * 4000;
           router.refresh();
         } else {
