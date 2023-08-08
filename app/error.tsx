0508916c982845b9cf0ddc469b285c43839431c2
sam101321 <sam010321@gmail.com>
2023-08-08 23:22:52 +0900

    fix(delete)게시글 삭제 타입개선

--- app/error.tsx
+++ app/error.tsx
@@ -3,7 +3,7 @@
 export default function Error({ error, reset }:any) {
   return (
     <div>
-      <h4>오 이런 에러남</h4>
+      <h4>에러 났습니다</h4>
       <button
         onClick={() => {
           reset();
--- pages/api/delete/[delete].ts
+++ pages/api/delete/[delete].ts
@@ -2,22 +2,24 @@ import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]";
+import { NextApiRequest, NextApiResponse } from "next";
 
 
-export default async function handler(요청: any, 응답: any) {
-  if (요청.query) {
-    let session = await getServerSession(요청, 응답, authOptions);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+   const postId = req.query._id as string;
+  if (postId) {
+    let session = await getServerSession(req, res, authOptions);
 
     let db = (await connectDB).db("forum");
     let find = await db
       .collection("post")
-      .findOne({ _id: new ObjectId(요청.query) });
+      .findOne({ _id: new ObjectId(postId) });
     if (session?.user?.email === find?.author) {
       let result = await db
         .collection("post")
-        .deleteOne({ _id: new ObjectId(요청.query) });
+        .deleteOne({ _id: new ObjectId(postId) });
       console.log(result);
-      응답.status(200).json("삭제완료");
+      res.status(200).json("삭제완료");
     }
   }
 }
